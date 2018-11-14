7570805b0f6f40981a8122de6786d12953ad98ca
Anton Kogan <34128946+chinhippy@users.noreply.github.com>
2018-11-13 20:01:53 -0600

    index.html - modeled after in-class activity

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDhp0rUoZr8w_uQrOrXoM0K8Xrfh0JKm7c",
+    authDomain: "train-scheduler-516e2.firebaseapp.com",
+    databaseURL: "https://train-scheduler-516e2.firebaseio.com",
+    projectId: "train-scheduler-516e2",
+    storageBucket: "",
+    messagingSenderId: "189327729806"
+  };
+  firebase.initializeApp(config);
